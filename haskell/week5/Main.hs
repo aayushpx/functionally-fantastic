ded445eb9c2c7589a08aed7c3ec7acc43023068b
Aayush Prakash <m.v.somaiya21@gmail.com>
2026-09-01 23:54:56 +1200

    lambda function implemented

--- haskell/week5/Main.hs
+++ haskell/week5/Main.hs
@@ -31,3 +31,43 @@ multiply x y = x*y
 double :: Int -> Int
 double  = multiply 2
 
+myMap :: (a -> b) -> [a] -> [b]
+myMap f []        = []
+myMap f (x:xs)    = f x : myMap f xs
+
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p []       = []
+myFilter p (x:xs)
+  | p x       = x : myFilter p xs 
+  | otherwise = myFilter p xs
+
+  -- high-order function mapfilter
+mapfilter :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+mapfilter p f []        = []
+mapfilter p f (x:xs)
+  | p x       = f x : mapfilter p f xs 
+  | otherwise = mapfilter p f xs
+
+mapfilter2 :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+mapfilter2 p f lst       = map f (filter p lst)
+
+mapfilter3 :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+mapfilter3 p f        = (map f . filter p)
+
+mapfilter4 :: (a -> Bool) -> (a -> b) -> [a] -> [b]
+mapfilter4 = \p -> \f -> \xs -> map f (filter p xs)
+
+{-
+*Main> mapfilter even (*10) [1..10]
+[20,40,60,80,100]
+
+*Main> mapfilter2 even (*10) [1..10]
+[20,40,60,80,100]
+
+*Main> mapfilter3 even (*10) [1..10]
+[20,40,60,80,100]
+
+*Main> mapfilter4 even (*10) [1..10]
+[20,40,60,80,100]
+-}
